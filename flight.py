9a3d9b0d5e031ed2d8822e16216aebdf7736ffa0
JesseMurah <kwekubrown234@gmail.com>
2022-09-13 00:48:43 +0100

    Flight booking portal for users

---
+++ flight.py
@@ -0,0 +1,24 @@
+class Flight():
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.passengers = []
+
+#add someone new to the flight
+
+    def add_passenger(self, name):
+        if not self.open_seats():
+            return False
+        self.passengers.append(name)   
+        return True     
+    def open_seats(self):
+        return self.capacity - len(self.passengers)
+
+flight = Flight(3)
+
+people = ["Jesse", "Kweku", "Joel", "Kingsley"]
+for person in people:
+    success = flight.add_passenger(person)
+    if success:
+        print(f"Added {person} to flight successfully.")
+    else:
+        print(f"No available seats for {person}.")
